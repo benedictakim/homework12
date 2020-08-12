3733fcbd1b9264061e466f01861a1f7989fa8937
Benn <61118590+benedictakim@users.noreply.github.com>
2020-08-11 23:04:34 -0400

    menu function but does not yet work

--- cms.js
+++ cms.js
@@ -18,5 +18,68 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  console.log("Successful connection to sql!");
+  employeetrackermenu;
 });
+
+function employeetrackermenu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        // "View All Employees By Department",
+        // "View All Employees By Manager",
+        "Add Employee",
+        // "Remove Employee",
+        "Update Employee Role",
+        // "Update Employee Manager",
+        "View All Roles",
+        "Add Role",
+        // "Remove Role",
+        "View All Deparments",
+        "Add Department"        
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View All Employees":
+        AllEmployees();
+        break;
+
+      case "Add Employee":
+        AddEmployee();
+        break;
+
+      case "Update Employee Role":
+        UpdateEmployeeRole();
+        break;
+
+      case "View All Roles":
+        AllRoles();
+        break;
+
+      case "Add Role":
+        AddRole();
+        break;
+        
+      case "View All Departments":
+        AllDepartments();
+        break;
+
+      case "Add Department":
+        AddDepartment();
+        break;
+      }
+    });
+}
+
+function AllEmployees () {
+  connection.query("SELECT * FROM employees", function(err, res) {
+    if (err) throw err; 
+    console.table(['First Name', 'Last Name'], res);
+    employeetrackermenu();
+  });
+}
